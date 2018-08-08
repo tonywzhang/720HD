e67529ea9b43ec06eba52cbc16f1ef2f0ab9961a
Tony <tonywzhang@gmail.com>
2018-08-07 17:20:07 -0700

    New Root Folder

---
+++ app/controllers/api/controllers_controller.rb
@@ -0,0 +1,2 @@
+class Api::ControllersController < ApplicationController
+end
---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+
+  root to:"static_pages#root"
+
+  namespace :api,defaults: {format: :json} do
+    resources :user, only: [:create]
+    resource :session, only: [:create, :destroy, :show]
+  end
+
+  #resources
+
+end
